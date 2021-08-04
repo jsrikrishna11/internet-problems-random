dd364d55411e8b1587d35f7dc2da92638db6c737
Sri Krishna J <srikrishna11@protonmail.ch>
2021-08-05 03:32:57 +0530

    added command line args feature

--- src/RotateMatrix.java
+++ src/RotateMatrix.java
@@ -2,10 +2,27 @@ public class RotateMatrix {
 private static void print2D(int matrix[][], int size){
     for (int row=0; row < size; row++){
         for(int col =0; col <size; col++){
-            System.out.print(matrix[row][col]);
+            System.out.print(matrix[row][col]+",");
         }
         System.out.print("\n");
     }
+}
+private static int[][] input2D(int size, String args[]){
+    int matrix[][] = new int[size][size];
+    //1,2,3,4,5,6,7,8,9
+    /*
+    1,2,3
+    4,5,6
+    7,8,9
+     */
+    int increment = 1;
+    for (int row=0; row < size; row++){
+        for(int col = 0; col < size; col++){
+            matrix[row][col] = Integer.parseInt(args[2*row+col+increment]);
+        }
+        increment++;
+    }
+    return matrix;
 }
     public static void main(String args[]){
         /*
@@ -19,9 +36,10 @@ private static void print2D(int matrix[][], int size){
         9,10,11,12      15,11,7,3
         13,14,15,16     16,12,8,4
          */
-        int nRowsColsSize = 3;
+
+        int nRowsColsSize = Integer.parseInt(args[0]);
         int nRowsCols = nRowsColsSize -1;
-        int input[][] = {{1,2,3},{4,5,6},{7,8,9}};
+        int input[][] = input2D(nRowsColsSize, args);
         int output[][] = new int[nRowsColsSize][nRowsColsSize];
         System.out.println("Input Matrix: \n");
         print2D(input, nRowsColsSize);
