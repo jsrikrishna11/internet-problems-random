9f5b99ab22f55411013a661007e2c1828aa344d8
Sri Krishna J <srikrishna11@protonmail.ch>
2021-08-11 11:46:57 +0530

    Dynamic programming. Grid movement ways. implemented memoizationa and overloaded move method.

--- src/dynamicprogramming/GridMove.java
+++ src/dynamicprogramming/GridMove.java
@@ -2,6 +2,7 @@ package dynamicprogramming;
 
 
 import java.util.ArrayDeque;
+import java.util.HashMap;
 import java.util.Stack;
 
 class Coordinates {
@@ -11,6 +12,26 @@ class Coordinates {
         this.y = y;
     }
 
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+    @Override
+    public int hashCode(){
+        return (new Integer(x)).hashCode()+(new Integer(y)).hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // TODO Auto-generated method stub
+        Coordinates b=(Coordinates) obj;
+
+        return b.getX()==x && b.getY() == y;
+    }
+
     @Override
     public String toString() {
         return "{" + x + ", "+ y + "}";
@@ -18,13 +39,43 @@ class Coordinates {
 }
 public class GridMove {
 
+    private static int move(int row, int col, int nRow, int nCol, Stack<Coordinates> path, HashMap<Coordinates,Integer> memo){
+        int ways = 0;
+        int moveRight = col + 1;
+        int moveDown = row + 1;
+        Coordinates currentNode = new Coordinates(row, col);
+        path.add(new Coordinates(row,col));
+        if(memo.containsKey(currentNode)){
+            return memo.get(currentNode).intValue();
+        }
+        if(row == nRow && col == nCol){
+            //System.out.println(path);
+            memo.put(currentNode, ways+1);
+            return ways+1;
+        }
+        if(row == nRow && col < nCol){
+            ways += move(row, moveRight, nRow, nCol, path, memo);
+        }
+        else if(row < nRow && col == nCol){
+            ways += move(moveDown, col, nRow, nCol, path, memo);
+        }else if(row < nRow && col < nCol){
+            ways += move(moveDown,col,nRow, nCol,path, memo);
+            path.pop();
+            ways += move(row, moveRight, nRow, nCol,path, memo);
+
+        }
+        path.pop();
+        memo.put(currentNode, ways);
+        return ways;
+    }
     private static int move(int row, int col, int nRow, int nCol, Stack<Coordinates> path){
         int ways = 0;
         int moveRight = col + 1;
         int moveDown = row + 1;
         path.add(new Coordinates(row,col));
+
         if(row == nRow && col == nCol){
-            System.out.println(path);
+            //System.out.println(path);
             return ways+1;
         }
         if(row == nRow && col < nCol){
@@ -50,8 +101,17 @@ public class GridMove {
 
         Do we use Memoization? - Yes
          */
+        final long startTime = System.currentTimeMillis();
         Stack<Coordinates> path = new Stack<>();
-        System.out.println(move(1,1,3,3,path));
+        HashMap<Coordinates,Integer> memo = new HashMap<>();
+        System.out.println(move(1,1,20,20,path,memo));
+        final long endTime = System.currentTimeMillis();
+        System.out.println("Total execution time for Memoization: " + (endTime - startTime));
+
+        final long startTime1 = System.currentTimeMillis();
+        System.out.println(move(1,1,15,15,path));
+        final long endTime1 = System.currentTimeMillis();
+        System.out.println("Total execution time for Pure Recursion: " + (endTime1 - startTime1));
 
     }
 }
